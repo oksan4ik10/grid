a90ae14321f6255a3a83f8974720cbb11b771b6e
Oksana Kalinina <ksan4ik92@gmail.com>
2021-02-23 20:01:27 +0300

    add js for slider

---
+++ script/script.js
@@ -0,0 +1,24 @@
+const btnPrev = document.querySelector('.btn__prev'),
+btnNext = document.querySelector('.btn__next'),
+slider = document.querySelector('.slider'),
+sliderItem = document.querySelectorAll('.slider__item');
+
+console.log(btnPrev,btnNext);
+
+count = 0;
+
+btnPrev.addEventListener('click',()=>{
+    count--;
+    if (count<0) {
+        count = 0;
+        return;
+    }
+    slider.prepend(sliderItem[count]);
+    
+});
+
+btnNext.addEventListener('click',()=>{
+    if (count == 12) count = 0;
+    slider.append(sliderItem[count]);
+    count++;
+})
